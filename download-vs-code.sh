a8652d3c3e31e15542e65c9bd5ace6f8f82d520a
Khalifah K. Shabazz <shabazzk@gmail.com>
2024-05-17 22:49:26 -0400

    add: Addtional Symbolic Link For Server
    Add another symbolic link for VS Code Server that is used with Remote
    SSH containers.

--- download-vs-code.sh
+++ download-vs-code.sh
@@ -91,7 +91,8 @@ install_server() {
     echo "%s" "setup symlinks"
     ln -s ~/.vscode-server/bin/"${commit_sha}" ~/.vscode-server/bin/default_version
     ln -s ~/.vscode-server/bin/"${commit_sha}" ~/.vscode/cli/servers/Stable-"${commit_sha}"/server
-    ln -s "${HOME}"/.vscode-server/bin/"${commit_sha}"/bin/code-server "${HOME}"/code-server
+    ln -s ~/.vscode-server/bin/"${commit_sha}" ~/.vscode-server/cli/servers/Stable-${commit_sha}/server
+    ln -s ~/.vscode-server/bin/"${commit_sha}"/bin/code-server ~/code-server
     echo "done"
 }
 
